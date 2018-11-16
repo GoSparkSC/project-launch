72713ba3e993e7f4ce47b0df06ccdf5c235fa29b
AdityaAgg <agga140@usc.edu>
2018-11-16 01:24:08 -0800

    some improvements

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,23 +1,78 @@
-var ctx = document.getElementById('my-chart').getContext('2d');
+var ctx = document.getElementById('gender-chart').getContext('2d');
 $(document).foundation();
 let data = {
-    datasets: [{
-        data: [10, 20, 30]
-    }],
+  datasets: [
+    {
+      data: [25, 75]
+    }
+  ],
 
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-        'Red',
-        'Yellow',
-        'Blue'
-    ]
+  // These labels appear in the legend and in the tooltips when hovering different arcs
+  labels: ['Male', 'Female']
 };
-var myPieChart = new Chart(ctx,{
-    type: 'pie',
-    data: data,
-    options: Chart.defaults.pie
-});
 
-$('.carousel').carousel({
-  interval: 1500
-})
+$('.carousel').carousel({interval: 1500})
+
+var ethinicity_chart_do = document.getElementById('ethnicity-chart').getContext('2d');
+
+let data_ethnicity = {
+  datasets: [
+    {
+      data: [20, 80]
+    }
+  ],
+
+  // These labels appear in the legend and in the tooltips when hovering different arcs
+  labels: ['African American', 'Hispanic']
+};
+var myPieChart = null;
+var ethnicity_pie_chart = null;
+
+$(document).ready(function() {
+  $('.toggle-button').click((obj) => {
+
+    obj = $(obj.target);
+    if (obj.hasClass("toggle")) {
+      obj.addClass("toggle-active");
+      obj.removeClass("toggle");
+
+    } else {
+      obj.addClass("toggle");
+      obj.removeClass("toggle-active");
+
+    }
+  });
+
+  $.fn.isInViewport = function() {
+    //console.log($(this))
+    if ($(this).length > 0) {
+      console.log("here")
+      var elementTop = $(this).offset().top;
+      var elementBottom = elementTop + $(this).outerHeight();
+
+      var viewportTop = $(window).scrollTop();
+      var viewportBottom = viewportTop + $(window).height();
+
+      return elementBottom > viewportTop && elementTop < viewportBottom;
+    }
+  };
+
+  $(window).on('resize scroll', function() {
+    if ($(".charts-information").isInViewport() && !myPieChart) {
+
+      myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: Chart.defaults.pie
+      });
+      setTimeout(function() {
+        ethnicity_pie_chart = new Chart(ethinicity_chart_do, {
+          type: 'pie',
+          data: data_ethnicity,
+          options: Chart.defaults.pie
+        });
+      }, 250);
+    } else {}
+  });
+
+});
